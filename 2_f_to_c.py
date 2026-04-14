d8861833e39aea50c9aca6e9e684cc24cc3f8e05
elhanmasalbekov9-eng <elhanmasalbekov9@gmail.com>
2026-04-14 07:38:24 +0600

    2_f_to_c.py

--- 2_f_to_c.py
+++ 2_f_to_c.py
@@ -1,3 +1,8 @@
+def fahrenheit_to_celsius(f):
+    return (f - 32) * 5/9
+
+
 f = float(input("Введите температуру в Фаренгейтах: "))
-c = (f - 32) * 5/9
+c = fahrenheit_to_celsius(f)
+
 print("Температура в Цельсиях:", c)
